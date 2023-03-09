0a2c464484d049b86e55dd2d750dc7f6debe1368
gacetta <78240758+gacetta@users.noreply.github.com>
2023-03-08 20:12:32 -0800

    update NavLinks
    Co-authored-by: Nattie Chan <nattie.chan@gmail.com>
    Co-authored-by: Jason Huang <jjhuang3@hotmail.com>
    Co-authored-by: Clark Pang <sondelius@gmail.com>
    Co-authored-by: Michael Gacetta <michael.gacetta@gmail.com>

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -6,14 +6,12 @@ function Navbar ({ user }) {
     <div className="navbarContainer">
       <nav className="navbar">
         <ul className="navlist">
-          {/* <NavLink to='/signup'>SignUp</NavLink> */}
           {!user && <li><NavLink to="/login">Login</NavLink></li>}
           {!user && <li><NavLink to="/signup">Sign up</NavLink></li>}
-          {user && <li><NavLink to="/boards">Boards '/boards'</NavLink></li>}
-          {user && <li><NavLink to="/">Home '/'</NavLink></li>}
+          {user && <li><NavLink to="/boards">My Boards</NavLink></li>}
         </ul>
         {user && <h1 className="navbarTitle">welcome {user.username}</h1>}
-        {user && <button className="logOut" onClick={() => (console.log('logging out user: ', user))}>LOG OUT</button>}
+        {user && <button className="logOut" onClick={() => (console.log('logging out user: ', user.username))}>LOG OUT</button>}
       </nav>
     </div>
   );
